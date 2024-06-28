8f0eca26a7f9124192cb8aa498314ea8ca9eb19d
Aniketrathore1 <aniketrathi03@gmail.com>
2024-06-28 11:24:57 +0530

    add files

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVZ05mL9i_4pfa2ETXF_rkk4GNMKS2gwU",
+  authDomain: "netflix-clone-63fd9.firebaseapp.com",
+  projectId: "netflix-clone-63fd9",
+  storageBucket: "netflix-clone-63fd9.appspot.com",
+  messagingSenderId: "221522925491",
+  appId: "1:221522925491:web:a2bd2a56ea48b426e61dc6"
+};
+
+
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzQyYzRhNTdiNmRkZWNhMzY5ZjFkZDAzZTEwZGYxNiIsIm5iZiI6MTcxOTU0OTczMy4wMzMzNCwic3ViIjoiNjY3ZTNkZDQwZDY1M2EwNDJlNDYwMDI5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.GWWz1UnaHeZdFIU8NOO05Ihm5Emr7UpFOCH7BIo8teI";
+
+  export {firebaseConfig, TMDB_Access_Key};
